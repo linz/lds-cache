b65059cdfdf5a6c3e8ebebbfdb13b4c28ac5b178
Blayne Chard <bchard@linz.govt.nz>
2021-09-14 12:46:32 +1200

    feat: schedule lambda to run every hour

--- infra/lds.export.cache.ts
+++ infra/lds.export.cache.ts
@@ -4,6 +4,8 @@ import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { StringParameter } from '@aws-cdk/aws-ssm';
 import { execFileSync } from 'node:child_process';
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 
 export class LdsExportCache extends Stack {
   constructor(scope?: Construct, id?: string, props?: StackProps) {
@@ -25,6 +27,11 @@ export class LdsExportCache extends Stack {
         GIT_VERSION: execFileSync('git', ['describe', '--tags', '--always', '--match', 'v*']).toString().trim(),
       },
     });
+
+    // Schedule the lambda to run every hour
+    const schedule = Schedule.cron({ minute: '0', hour: '1' });
+    const eventRule = new Rule(this, 'scheduleRule', { schedule });
+    eventRule.addTarget(new LambdaFunction(lambda));
 
     cacheBucket.grantReadWrite(lambda);
     new CfnOutput(this, 'CacheBucket', { value: cacheBucket.bucketName });
