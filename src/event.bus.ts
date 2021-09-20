3e11cf87b033bb529d1e80f229566b8c8cfb46c1
Wentao Kuang <wt.kuang@icloud.com>
2021-09-20 17:10:34 +1200

    feat: introduce event bus and push ingested event into it. (#13)
    https://github.com/linz/lds-export-cache/pull/13

---
+++ src/event.bus.ts
@@ -0,0 +1,42 @@
+import { LambdaRequest } from '@linzjs/lambda';
+import EventBridge from 'aws-sdk/clients/eventbridge.js';
+import { KxDataset } from './kx.dataset.js';
+
+interface DatasetIngestedEvent {
+  datasetId: number;
+  versionId: number;
+  type: string;
+}
+
+export class AwsEventBridgeBus {
+  eventBusArn: string;
+  eventBridge: EventBridge;
+
+  constructor() {
+    const eventBusArn = process.env['EVENT_BUS_ARN'];
+    if (eventBusArn == null) throw new Error('Missing $EVENT_BUS_ARN');
+    this.eventBusArn = eventBusArn;
+    this.eventBridge = new EventBridge();
+  }
+
+  async putDatasetIngestedEvent(req: LambdaRequest, dataset: KxDataset): Promise<void> {
+    const version = await dataset.getLatestVersion();
+    const event: DatasetIngestedEvent = {
+      datasetId: dataset.id,
+      versionId: version.version.id,
+      type: 'Ingested',
+    };
+    const entry: EventBridge.PutEventsRequestEntry = {
+      Time: new Date(dataset.info.published_at),
+      Source: 'nz.govt.linz.lds-cache',
+      EventBusName: this.eventBusArn,
+      Detail: JSON.stringify(event),
+      DetailType: 'Dataset:Ingested',
+    };
+    const res = await this.eventBridge.putEvents({ Entries: [entry] }).promise();
+    if (res.Entries != null)
+      for (const event of res.Entries) {
+        req.log.info({ event, eventId: event.EventId }, 'EventBus:Send');
+      }
+  }
+}
