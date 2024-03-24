e89ffdcac5b1e44415e6b6aeb8e73b51a3a53179
Blayne Chard <bchard@linz.govt.nz>
2024-03-25 09:44:55 +1300

    feat(infra): add linz specific application tags (#1047)
    #### Motivation
    
    LINZ has a number of common tags to help determine what the
    infrastructure is related to
    
    #### Modification
    
    apply common tags.
    
    #### Checklist
    
    _If not applicable, provide explanation of why._
    
    - [ ] Tests updated
    - [ ] Docs updated
    - [ ] Issue linked in Title

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,7 +1,16 @@
-import { App } from 'aws-cdk-lib';
+import { App, Tags } from 'aws-cdk-lib';
 
+import { getGitBuildInfo } from './build.js';
 import { LdsExportCache } from './lds.export.cache.js';
 
 const app = new App();
 
-new LdsExportCache(app, 'LdsExporter', { env: { region: 'ap-southeast-2' } });
+const lambda = new LdsExportCache(app, 'LdsExporter', { env: { region: 'ap-southeast-2' } });
+
+Tags.of(lambda).add('linz:app:name', 'lds-cache');
+Tags.of(lambda).add('linz:app:version', getGitBuildInfo().version);
+Tags.of(lambda).add('linz:environment', 'prod');
+Tags.of(lambda).add('linz:git:hash', getGitBuildInfo().hash);
+if (process.env['GITHUB_REPOSITORY']) Tags.of(lambda).add('linz:git:repository', process.env['GITHUB_REPOSITORY']);
+Tags.of(lambda).add('linz:security:classification', 'unclassified');
+Tags.of(lambda).add('linz:build:id', getGitBuildInfo().buildId);
